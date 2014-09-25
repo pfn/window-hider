c62f0f7eefb7cba4a300980e098e256f3cc2ad8d
Perry Nguyen <pfnguyen@hanhuy.com>
2014-09-25 09:26:09 -0700

    Draw cursor icon, focus restored window

--- hide-app.c
+++ hide-app.c
@@ -34,12 +34,17 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     static HWND targetHwnd;
     static HBRUSH hbrstatic;
     static NOTIFYICONDATA iconData;
+    static HWND iconStatic;
+    static HWND parent = NULL;
+    if (!parent) parent = hWnd;
+
     if (msg == WM_TRAY_ICON_NOTIFYICON) {
 
         switch ((UINT)lParam) {
             case WM_LBUTTONUP:
                 Shell_NotifyIcon(NIM_DELETE, &iconData);
                 ShowWindow(targetHwnd, SW_RESTORE);
+                SetForegroundWindow(targetHwnd);
                 PostQuitMessage(0);
                 break;
         }
@@ -47,6 +52,23 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     }
     RECT r;
     switch (msg) {
+        case WM_PAINT:
+            if (hWnd == iconStatic) {
+                RECT rc;
+                PAINTSTRUCT ps;
+
+                HDC hdc = BeginPaint(hWnd, &ps);
+                HCURSOR cursor = LoadCursor(NULL, IDC_CROSS);
+                GetClientRect(hWnd, &rc);
+                int w = GetSystemMetrics(SM_CXCURSOR);
+                DrawIcon(hdc, (rc.right - rc.left - w) / 2, 0, cursor);
+                SetBkMode(hdc, TRANSPARENT);
+                EndPaint(hWnd, &ps);
+                DeleteObject(cursor);
+                return 0;
+            } else {
+                return DefWindowProc(hWnd, msg, wParam, lParam);
+            }
         case WM_CTLCOLORSTATIC: {
             HDC hdcStatic = (HDC) wParam;
             DWORD c = GetSysColor(COLOR_WINDOW);
@@ -58,12 +80,28 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             return (INT_PTR)hbrstatic;
         }
         case WM_CREATE:
-            GetWindowRect(hWnd, &r);
-            CreateWindowEx(0, "STATIC",
-                    "Drag the cursor to any window to hide it",
-                    WS_CHILD | WS_VISIBLE,
-                    24, 24, r.right - r.left - 48, r.bottom - r.top - 48,
-                    hWnd, NULL, GetModuleHandle(NULL), NULL);
+            if (hWnd != iconStatic) {
+                GetWindowRect(hWnd, &r);
+                CreateWindow("STATIC",
+                        "Drag the cursor into any window to hide it",
+                        WS_CHILD | WS_VISIBLE | SS_CENTER,
+                        0, 16, r.right - r.left, 64,
+                        hWnd, NULL, GetModuleHandle(NULL), NULL);
+
+                iconStatic = CreateWindow("Window Hider Static",
+                        "Window Hider Static",
+                        WS_CHILD | WS_VISIBLE,
+                        0, 64, r.right - r.left, r.bottom - r.top - 96,
+                        hWnd, NULL, GetModuleHandle(NULL), NULL);
+                SetWindowLongPtr(iconStatic,
+                        GWLP_WNDPROC, (INT_PTR) WindowProc);
+                if (!iconStatic) {
+                    MessageBox(hWnd,
+                            error(),
+                            TEXT("Error"),
+                            MB_ICONERROR | MB_OK);
+                }
+            }
             break;
         case WM_DESTROY:
             DeleteObject(hbrstatic);
@@ -72,10 +110,9 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         case WM_CLOSE:
             DestroyWindow(hWnd);
             break;
-        case WM_CAPTURECHANGED:
-            break;
         case WM_LBUTTONDOWN:
             SetCapture(hWnd);
+            ShowWindow(iconStatic, SW_HIDE);
             SetCursor(LoadCursor(NULL, IDC_CROSS));
             break;
         case WM_LBUTTONUP:
@@ -86,8 +123,9 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             ClientToScreen(hWnd, &p);
             HWND target = ChildWindowFromPointEx(GetDesktopWindow(), p,
                     CWP_SKIPDISABLED | CWP_SKIPINVISIBLE | CWP_SKIPTRANSPARENT);
-            if (target == hWnd) {
-                MessageBox(NULL,
+            ShowWindow(iconStatic, SW_RESTORE);
+            if (target == parent) {
+                MessageBox(hWnd,
                         TEXT("Unable to hide self"),
                         TEXT("Error"),
                         MB_ICONERROR | MB_OK);
@@ -113,10 +151,10 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
                 iconData.dwInfoFlags = 0x4; // NIIF_USER
                 Shell_NotifyIcon(NIM_ADD, &iconData);
                 ShowWindow(target, SW_HIDE);
-                ShowWindow(hWnd, SW_HIDE);
+                ShowWindow(parent, SW_HIDE);
                 targetHwnd = target;
             } else {
-                MessageBox(NULL,
+                MessageBox(hWnd,
                         TEXT("Unable to identify window, try again"),
                         TEXT("ChildWindowFromPoint Error"),
                         MB_ICONERROR | MB_OK);
@@ -140,32 +178,38 @@ int WINAPI WinMain(HINSTANCE hInstance,
     MSG msg;
 
     WNDCLASSEX wc;
+    ZeroMemory(&wc, sizeof(wc));
     wc.cbSize        = sizeof(wc);
-    wc.style         = 0;
     wc.lpfnWndProc   = &WindowProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
     wc.hInstance     = hInstance;
-    wc.lpszMenuName  = NULL;
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.hIcon         = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TRAYICON));
     wc.hIconSm       = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TRAYICON));
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.lpszClassName = MainWndClass;
 
+    WNDCLASS wc2;
+    ZeroMemory(&wc2, sizeof(wc2));
+    wc2.style = CS_HREDRAW | CS_VREDRAW;
+    wc2.lpfnWndProc = DefWindowProc;
+    wc2.hInstance = hInstance;
+    wc2.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+    wc2.lpszClassName = TEXT("Window Hider Static");
+    wc2.hCursor = LoadCursor(NULL, IDC_ARROW);
+
     INITCOMMONCONTROLSEX icc;
     icc.dwSize = sizeof(icc);
     icc.dwICC  = ICC_WIN95_CLASSES;
     InitCommonControlsEx(&icc);
 
-    if (!RegisterClassEx(&wc)) {
+    if (!RegisterClassEx(&wc) || !RegisterClass(&wc2)) {
         LPTSTR err = error();
         MessageBox(NULL,
                 err, TEXT("RegisterClassEx Error"), MB_ICONERROR | MB_OK);
         return 0;
     }
 
-    hWnd = CreateWindowEx(0, MainWndClass, MainWndClass,
+    hWnd = CreateWindow(MainWndClass, MainWndClass,
             WS_OVERLAPPEDWINDOW ^ (WS_SIZEBOX|WS_MAXIMIZEBOX|WS_MINIMIZEBOX),
             CW_USEDEFAULT, CW_USEDEFAULT,
             320, 200, NULL, NULL, hInstance, NULL);
@@ -173,7 +217,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     if (!hWnd) {
         LPTSTR err = error();
         MessageBox(NULL,
-                err, TEXT("CreateWindowEx Error"), MB_ICONERROR | MB_OK);
+                err, TEXT("CreateWindow Error"), MB_ICONERROR | MB_OK);
         return 0;
     }
 
